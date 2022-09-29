22253ccdc8ce93cf5482988fddde5d4602aef719
vitormoraes96 <vitor.ribeirom@gmail.com>
2022-09-28 21:45:22 -0400

    must insert VI and return 6

--- __tests__/romanNumbers.test.js
+++ __tests__/romanNumbers.test.js
@@ -4,4 +4,16 @@ describe("Roman Numbers", () => {
   it("must insert I and return 1", () => {
     expect(romanNumbers("I")).toBe(1);
   });
+
+  it("must insert V and return 5", () => {
+    expect(romanNumbers("V")).toBe(5);
+  });
+
+  it("must insert II and return 2", () => {
+    expect(romanNumbers("II")).toBe(2);
+  });
+
+  it("must insert VI and return 6", () => {
+    expect(romanNumbers("VI")).toBe(6);
+  });
 });
--- src/romanNumbers.js
+++ src/romanNumbers.js
@@ -1,3 +1,11 @@
-export default (number) => {
-  return 1;
+export default (numbers) => {
+  let sumNumber = 0;
+  const listNumbers = numbers.split("");
+  listNumbers.map((number) => (sumNumber += numbersRoman[number]));
+  return sumNumber;
+};
+
+const numbersRoman = {
+  I: 1,
+  V: 5,
 };
